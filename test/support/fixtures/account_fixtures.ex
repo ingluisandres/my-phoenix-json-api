3ee60de278bceeb6afd4d87023241c2f8a596538
Andrés Contreras <ingluisandres3@gmail.com>
2022-07-30 23:00:32 -0500

    Initial commit

---
+++ test/support/fixtures/account_fixtures.ex
@@ -0,0 +1,26 @@
+defmodule MyApp.AccountFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `MyApp.Account` context.
+  """
+
+  @doc """
+  Generate a unique user email.
+  """
+  def unique_user_email, do: "some email#{System.unique_integer([:positive])}"
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        email: unique_user_email(),
+        is_active: true
+      })
+      |> MyApp.Account.create_user()
+
+    user
+  end
+end
